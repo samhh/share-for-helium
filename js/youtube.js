4c7e85e7e33d6d98d79e7d5bb4b51196f05bfbb1
Sam A. Horvath-Hunt <samhh.web@gmail.com>
2016-01-12 17:55:04 +0000

    Add check for thumbnails again on new page load (per YouTube being SPA)

--- js/youtube.js
+++ js/youtube.js
@@ -2,16 +2,34 @@
 
 let title = 'Share with Helium'
 
-let el = document.getElementsByClassName('addto-watch-later-button')
+// Check for video thumbnails again when accessing new page, call once on load
+// Done this way as YouTube works as a SPA and spfdone is called on page change
+// Not checking for page name as needs to run again on "refresh" anyway
+document.addEventListener('spfdone', addButtons)
+addButtons()
 
-for (let i = 0; i < el.length; i++) {
-  let videoID = el[i].getAttribute('data-video-ids')
+function addButtons() {
+  let vids = document.getElementsByClassName('addto-button')
 
-  let insertedHTML = `
-    <a href="helium://https://www.youtube.com/watch?v=${videoID}">
-      <button class="sfh__youtube__share-in-thumbnail yt-uix-button yt-uix-button-size-small yt-uix-button-default">${title}</button>
-    </a>
-  `
+  if (vids.length === 0) return
 
-  el[i].insertAdjacentHTML('afterend', insertedHTML)
+  for (let i = 0; i < vids.length; i++) {
+    let triggeredClassName = 'sfh--triggered'
+
+    // Only run function if element has not already been accessed
+    if (vids[i].classList.contains(triggeredClassName)) return
+
+    let videoID = vids[i].getAttribute('data-video-ids')
+
+    let insertedHTML = `
+      <a href="helium://https://www.youtube.com/watch?v=${videoID}">
+        <button class="sfh__youtube__share-in-thumbnail yt-uix-button yt-uix-button-size-small yt-uix-button-default">${title}</button>
+      </a>
+    `
+
+    vids[i].insertAdjacentHTML('afterend', insertedHTML)
+
+    // Tag video thumbnail as having been found previously for future runs
+    vids[i].classList.add(triggeredClassName)
+  }
 }
