a52fa4842a895cb72974466017626a556b8c4da2
Sam A. Horvath-Hunt <samhh.web@gmail.com>
2016-03-23 17:41:44 +0000

    Add YouTube functionality for all newly loaded videos (mutation observation)

--- js/youtube.js
+++ js/youtube.js
@@ -2,26 +2,44 @@
 
 let title = 'Share with Helium'
 
-// Check for video thumbnails again when accessing new page, call once on load
-// Done this way as YouTube works as a SPA and spfdone is called on page change
-// Not checking for page name as needs to run again on "refresh" anyway
-document.addEventListener('spfdone', function() {
-  addButtons(document.getElementsByClassName('addto-button'))
+// Run once on page load
+addButtons(document.querySelectorAll('.addto-button'))
+
+// Watch for any new videos that are loaded and add button to them too
+let observer = new MutationObserver(function(mutations) {
+  let once = false
+
+  mutations.forEach(function(mutation) {
+    // Only continue if mutation is to tree of nodes
+    if (mutation.type !== 'childList') return
+
+    Array.from(mutation.addedNodes).forEach(function(node) {
+      // Only continue if node is an element
+      if (node.nodeType !== 1) return
+
+      console.log(node)
+
+      if (node.querySelectorAll('.addto-button')) addButtons(node.querySelectorAll('.addto-button'))
+    })
+  })
 })
 
-// Run once on page load
-addButtons(document.getElementsByClassName('addto-button'))
+observer.observe(document, {
+  childList: true,
+  subtree: true
+})
 
+// Add button to each of an array of nodes
 function addButtons(vids) {
-  if (vids.length === 0) return
+  if (!vids) return
 
-  for (let i = 0; i < vids.length; i++) {
+  Array.from(vids).forEach((vid) => {
     let triggeredClassName = 'sfh--triggered'
 
     // Exit loop if provided object isn't an element or if the element has already been accessed
-    if (vids[i].nodeType !== 1 || vids[i].classList.contains(triggeredClassName)) return
+    if (vid.nodeType !== 1 || vid.classList.contains(triggeredClassName)) return
 
-    let videoID = vids[i].getAttribute('data-video-ids')
+    let videoID = vid.getAttribute('data-video-ids')
 
     let insertedHTML = `
       <a href="helium://https://www.youtube.com/watch?v=${videoID}" class="sfh__youtube__share-in-thumbnail-anchor">
@@ -29,9 +47,9 @@ function addButtons(vids) {
       </a>
     `
 
-    vids[i].insertAdjacentHTML('afterend', insertedHTML)
+    vid.insertAdjacentHTML('afterend', insertedHTML)
 
     // Tag video thumbnail as having been found previously for future runs
-    vids[i].classList.add(triggeredClassName)
-  }
+    vid.classList.add(triggeredClassName)
+  })
 }
