2d0a7b4d3c1d1c6603ec0f14929f2efe9bae4c93
Sam A. Horvath-Hunt <samhh.web@gmail.com>
2017-02-13 17:15:50 +0000

    Fix Twitch

--- js/twitch.js
+++ js/twitch.js
@@ -1,35 +1,65 @@
 'use strict'
 
-// Chrome doesn't support NodeList.prototype[Symbol.iterator] natively yet - this is a dead easy fix for that to allow for..of loops on NodeLists
-NodeList.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator]
+const insertAfter = (newNode, refNode) => {
+  refNode.parentNode.insertBefore(newNode, refNode.nextSibling)
+}
 
-let title = 'Share with Helium'
+const title = 'Share with Helium'
 
-let triggeredClassName = 'sfh--triggered'
-let streamThumbClassName = 'sfh__thumb--stream'
-let vidThumbClassName = 'sfh__thumb--video'
-let targetStreams = `.streams .stream .content .thumb .cap:not(.${triggeredClassName})`
-let targetVids = `.videos .video .content .thumb .cap:not(.${triggeredClassName})`
+const triggeredClassName = 'sfh--triggered'
+const streamThumbClassName = 'sfh__thumb--stream'
+const vidThumbClassName = 'sfh__thumb--video'
+const targetStreams = `.streams .stream .content .thumb .cap:not(.${triggeredClassName})`
+const targetVids = `.videos .video .content .thumb .cap:not(.${triggeredClassName})`
+
+// Add button to each of an array of nodes
+const addButtons = (type, vids) => {
+  if (!['streams', 'vids'].includes(type)) return console.error(`SFH bad type provided: ${type}`)
+
+  if (!vids) return
+
+  for (const vid of vids) {
+    // Only proceed if node is an element
+    if (vid.nodeType !== 1) continue
+
+    const key = type === 'streams' ? 'twitch.tv/' : '/v/'
+    const strIndex = vid.href.indexOf(key)
+    const id = vid.href.substring(key.length + strIndex)
+
+    const magicLink = (type === 'streams' ? 'https://player.twitch.tv/?channel=' : 'https://player.twitch.tv/?video=v') + id
+
+    let newBtnEl = document.createElement('a')
+    newBtnEl.className = type === 'streams' ? streamThumbClassName : vidThumbClassName
+    newBtnEl.href = `helium://${magicLink}`
+    newBtnEl.textContent = title
+
+    console.log(vid)
+    insertAfter(newBtnEl, vid)
+
+    // Tag thumbnail as having been found previously for future runs
+    vid.classList.add(triggeredClassName)
+  }
+}
 
 // Run through each once on page load
-addButtons(document.querySelectorAll('streams', targetStreams))
-addButtons(document.querySelectorAll('vids', targetVids))
+addButtons('streams', document.querySelectorAll(targetStreams))
+addButtons('vids', document.querySelectorAll(targetVids))
 
 // Watch for any new videos that are loaded and add button to them too
 let observer = new MutationObserver((mutations) => {
-  for (let mutation of mutations) {
-    // Only continue if mutation is to tree of nodes
+  for (const mutation of mutations) {
+    // Only proceed if mutation is to tree of nodes
     if (mutation.type !== 'childList') continue
 
-    for (let node of mutation.addedNodes) {
-      // Only continue if node is an element
+    for (const node of mutation.addedNodes) {
+      // Only proceed if node is an element
       if (node.nodeType !== 1) continue
 
-      let targetStreamsLookup = node.querySelectorAll(targetStreams)
-      if (targetStreamsLookup) addButtons('streams', targetStreamsLookup)
+      const targetStreamsLookup = node.querySelectorAll(targetStreams)
+      if (targetStreamsLookup.length) addButtons('streams', targetStreamsLookup)
 
-      let targetVidsLookup = node.querySelectorAll(targetVids)
-      if (targetVidsLookup) addButtons('vids', targetVidsLookup)
+      const targetVidsLookup = node.querySelectorAll(targetVids)
+      if (targetVidsLookup.length) addButtons('vids', targetVidsLookup)
     }
   }
 })
@@ -39,49 +69,12 @@ observer.observe(document, {
   subtree: true
 })
 
-// Add button to each of an array of nodes
-function addButtons (type, vids) {
-  if (type !== 'streams' && type !== 'vids') return console.error(`SFH bad type provided: ${type}`)
-
-  if (!vids) return
-
-  for (let vid of vids) {
-    // Only continue if node is an element
-    if (vid.nodeType !== 1) continue
-
-    let thisThumb = type === 'streams' ? streamThumbClassName : vidThumbClassName
-
-    let magicLink
-    if (type === 'streams') {
-      let length = 'twitch.tv/'.length
-      let strIndex = vid.href.indexOf('twitch.tv/')
-      let username = vid.href.substring(length + strIndex)
-
-      magicLink = `https://player.twitch.tv/?channel=${username}`
-    } else {
-      let length = '/v/'.length
-      let strIndex = vid.href.indexOf('/v/')
-      let videoId = vid.href.substring(length + strIndex)
-
-      magicLink = `https://player.twitch.tv/?video=v${videoId}`
-    }
-
-    let insertedHTML = `
-      <a href="helium://${magicLink}" class="${thisThumb}">
-        ${title}
-      </a>
-    `
-
-    vid.insertAdjacentHTML('afterbegin', insertedHTML)
-
-    let insertedEl = vid.querySelector(`.${thisThumb}`)
-
-    insertedEl.addEventListener('click', (e) => {
-      // Prevent some other event handler on Twitch from taking us to the URL of the video below our button
-      e.stopImmediatePropagation()
-    })
-
-    // Tag video thumbnail as having been found previously for future runs
-    vid.classList.add(triggeredClassName)
-  }
-}
+// Static and featured videos
+// let channelVid = document.querySelector('#subscribe_button')
+// if (channelVid) {
+//   let insertedHTML = `
+//     <a href="helium://TODO" class="button primary">Share with Helium</a>
+//   `
+//
+//   channelVid.insertAdjacentHTML('afterend', insertedHTML)
+// }
